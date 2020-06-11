7df5fdee5deb06ad737c024dd6137d56909c4023
PAVULURI <PAVULURI@PAVULURI>
2020-06-11 11:57:27 +0530

    Source Code for Ex4(b) in LanguageBasics

--- MileStone1/src/com/wipro/languagebasics/Ex4B.java
+++ MileStone1/src/com/wipro/languagebasics/Ex4B.java
@@ -1,22 +1,23 @@
-// Using Command Line Arguments
-
 package com.wipro.languagebasics;
 
 public class Ex4B {
 public static void main(String[] args) {
 	int first = Integer.parseInt(args[0]);
 	int second = Integer.parseInt(args[1]);
-	int result1;
-	int result2;
-	result1 = first % 10;
-	result2 = second % 10;
-	if(result1 == result2)
+	String s = lastdigit(first,second);
+	System.out.println(s);
+}
+
+public static String lastdigit(int one, int two)
+{
+	if(one % 10 == two % 10)
 	{
-		System.out.println("true");
+	return "true";
 	}
 	else
 	{
-		System.out.println("false");
+		return "false";
 	}
+	
 }
 }
