4a5aa07434097ff074ca95fbc6077ff845d8b080
PAVULURI <PAVULURI@PAVULURI>
2020-06-11 10:42:33 +0530

    Source Code of Ex3 in LanguageBasics

---
+++ MileStone1/src/com/wipro/languagebasics/Ex3.java
@@ -0,0 +1,11 @@
+package com.wipro.languagebasics;
+
+public class Ex3 {
+public static void main(String[] args) {
+	int first = Integer.parseInt(args[0]);
+	int second = Integer.parseInt(args[1]);
+	int result;
+	result = first + second;
+	System.out.println("The sum of"+ " "+ first + " " + "and" + " " + second + " " + "is" + " " + result);
+}
+}
