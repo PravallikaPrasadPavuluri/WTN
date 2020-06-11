41061d26ff0c69d06cb2bc47eafc286ab2717bc3
PAVULURI <PAVULURI@PAVULURI>
2020-06-11 13:47:11 +0530

    Source Code for Ex1(a) in Flow Control Statements

---
+++ MileStone1/src/com/wipro/FlowControlStatements/Q1.java
@@ -0,0 +1,20 @@
+package com.wipro.FlowControlStatements;
+
+public class Q1 {
+	
+		public static void main(String[] args) {
+		int input = Integer.parseInt(args[0]);
+		if(input > 0)
+		{
+			System.out.println("It is a Positive Number");
+		}
+		else if(input == 0)
+		{
+			System.out.println("It is Zero");
+		}
+		else
+		{
+			System.out.println("It is a Negative Number");
+		}
+		}
+}
