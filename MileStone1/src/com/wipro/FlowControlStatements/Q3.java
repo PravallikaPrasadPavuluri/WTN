d4ae3b15408d5d15f750eab70fcae7b093455ca8
PAVULURI <PAVULURI@PAVULURI>
2020-06-11 15:10:33 +0530

    Source Code for Q3 in Flow Control Statements

---
+++ MileStone1/src/com/wipro/FlowControlStatements/Q3.java
@@ -0,0 +1,15 @@
+package com.wipro.FlowControlStatements;
+
+public class Q3 {
+public static void main(String[] args) {
+	int number = Integer.parseInt(args[0]);
+	if(number % 2 == 0)
+	{
+		System.out.println("It is an Even Number");
+	}
+	else
+	{
+		System.out.println("It is an Odd Number");
+	}
+}
+}
