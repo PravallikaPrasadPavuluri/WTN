d51dfd16877b3ee66afcb44f5577c92af4bac6cf
PAVULURI <PAVULURI@PAVULURI>
2020-06-11 15:01:55 +0530

    Source Code for Q1(a) in Flow Control Statements

--- MileStone1/src/com/wipro/FlowControlStatements/Q1.java
+++ MileStone1/src/com/wipro/FlowControlStatements/Q1.java
@@ -1,5 +1,19 @@
 package com.wipro.FlowControlStatements;
 
 public class Q1 {
-
+public static void main(String[] args) {
+	int number = Integer.parseInt(args[0]);
+	if(number > 0)
+	{
+		System.out.println("It is a Positive Number");
+	}
+	else if(number == 0)
+	{
+		System.out.println("It is Zero");
+	}
+	else
+	{
+		System.out.println("It is Negative Number");
+	}
+}
 }
